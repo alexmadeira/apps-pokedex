39ac77df4edcc3a535c898eabaa22de25b9224d5
Alex Madeira <alexmadeira@MacBook-Pro-de-Alex.local>
2019-09-22 16:11:36 -0300

    teste size card

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -10,7 +10,7 @@ export const Container = styled(Link)`
   flex: 1;
   border-radius: 5px;
   margin: 10px;
-  min-width: 300px;
+  min-width: 200px;
   text-decoration: none;
   img {
     filter: grayscale(90%);
